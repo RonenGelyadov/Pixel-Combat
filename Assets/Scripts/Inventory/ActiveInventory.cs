c5b0630bd7533e1196753d4fc811faba6534bc9c
RonenGelyadov <ronengtv@gmail.com>
2025-03-26 23:16:03 +0200

    26.3.25 23:00

---
+++ Assets/Scripts/Inventory/ActiveInventory.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+public class ActiveInventory : MonoBehaviour
+{
+	int activeSlotIndexNum = 0;
+
+	PlayerControls playerControls;
+
+	void Awake()
+	{
+		playerControls = new PlayerControls();
+	}
+
+	void Start()
+	{
+		playerControls.Inventory.Keyboard.performed += ctx => ToggleActiveSlot((int)ctx.ReadValue<float>());
+	}
+
+	void OnEnable()
+	{
+		playerControls.Enable();
+	}
+
+	void ToggleActiveSlot(int numValue)
+	{
+		ToggleActiveHighlight(numValue - 1);
+	}
+
+	void ToggleActiveHighlight(int indexNum)
+	{
+		activeSlotIndexNum = indexNum;
+
+		foreach (Transform inventorySlot in this.transform)
+		{
+			inventorySlot.GetChild(0).gameObject.SetActive(false);
+		}
+
+		this.transform.GetChild(indexNum).GetChild(0).gameObject.SetActive(true);
+	}
+}
+
