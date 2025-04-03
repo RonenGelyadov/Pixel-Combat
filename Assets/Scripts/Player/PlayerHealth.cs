3d4613e78d16d4f906eb9a4e094f249609fa20b9
Ronen Gelyadov <ronengtv@gmail.com>
2025-04-03 09:21:38 +0300

    3.4.25 09:00

---
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+	[SerializeField] int maxHealth = 3;
+	[SerializeField] float knockBackThrustAmount = 10f;
+	[SerializeField] float damageRecoveryTime = 1f;
+
+	int currentHealth;
+	bool canTakeDamage = true;
+
+	Knockback knockback;
+	Flash flash;
+
+	void Awake()
+	{
+		knockback = GetComponent<Knockback>();
+		flash = GetComponent<Flash>();
+	}
+
+	void Start()
+	{
+		currentHealth = maxHealth;
+	}
+
+	void OnCollisionStay2D(Collision2D collision)
+	{
+		EnemyAI enemy = collision.gameObject.GetComponent<EnemyAI>();
+
+		if (enemy && canTakeDamage)
+		{
+			TakeDamage(1);
+			knockback.GetKnockedBack(collision.gameObject.transform, knockBackThrustAmount);
+			StartCoroutine(flash.FlashRoutine());
+		}
+	}
+
+	void TakeDamage(int damage)
+	{
+		canTakeDamage = false;
+		currentHealth -= damage;
+		StartCoroutine(DamageRecoveryRoutine());
+	}
+
+	IEnumerator DamageRecoveryRoutine()
+	{
+		yield return new WaitForSeconds(damageRecoveryTime);
+		canTakeDamage = true;
+	}
+}
