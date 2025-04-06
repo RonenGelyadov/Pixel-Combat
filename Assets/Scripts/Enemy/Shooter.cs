a80d31a085bc706c388b7aa9e340b57a37d0567c
Ronen Gelyadov <ronengtv@gmail.com>
2025-04-06 17:05:11 +0300

    6.4.25 17:00

--- Assets/Scripts/Enemy/Shooter.cs
+++ Assets/Scripts/Enemy/Shooter.cs
@@ -6,6 +6,9 @@ public class Shooter : MonoBehaviour, IEnemy
 	[SerializeField] GameObject bulletPrefab;
 	[SerializeField] float bulletMoveSpeed;
 	[SerializeField] int burstCount;
+	[SerializeField] int projectilesPerBurst;
+	[SerializeField] [Range(0, 359)] float angleSpread;
+	[SerializeField] float startingDistance = 0.1f;
 	[SerializeField] float timeBetweenBursts;
 	[SerializeField] float restTime = 1f;
 	// [SerializeField] float range = 5f;
@@ -23,13 +26,15 @@ public class Shooter : MonoBehaviour, IEnemy
 	IEnumerator ShootRoutine()
 	{
 		isShooting = true;
+		
+		Vector2 targetDirection = PlayerController.Instance.transform.position - transform.position;
+		float targetAngle = Mathf.Atan2(targetDirection.y, targetDirection.x);
 
 		for (int i = 0; i < burstCount; i++)
 		{
-			Vector2 targerDirection = PlayerController.Instance.transform.position - transform.position;
 
 			GameObject newBullet = Instantiate(bulletPrefab, transform.position, Quaternion.identity);
-			newBullet.transform.right = targerDirection;
+			newBullet.transform.right = targetDirection;
 
 			if (newBullet.TryGetComponent(out Projectile projectile))
 			{
