f08aa17f139942052a2aede9faf10ab280c8bcf9
Ronen Gelyadov <ronengtv@gmail.com>
2025-04-10 16:12:56 +0300

    10.4.25 16:00

--- Assets/Scripts/Misc/PickUpSpawner.cs
+++ Assets/Scripts/Misc/PickUpSpawner.cs
@@ -2,10 +2,29 @@ using UnityEngine;
 
 public class PickUpSpawner : MonoBehaviour
 {
-	[SerializeField] GameObject goldCoinPrefab;
+	[SerializeField] GameObject goldCoin, healthGlobe, staminaGlobe;
 
 	public void DropItems()
 	{
-		Instantiate(goldCoinPrefab, transform.position, Quaternion.identity);
+		int randomNum = Random.Range(1, 4);
+
+		if (randomNum == 1)
+		{
+			Instantiate(healthGlobe, transform.position, Quaternion.identity);
+		}
+		else if (randomNum == 2)
+		{
+			Instantiate(staminaGlobe, transform.position, Quaternion.identity);
+		}
+		else if (randomNum == 3)
+		{
+			int randomAmountOfGold = Random.Range(1, 4);
+
+			for (int i = 0; i < randomAmountOfGold; i++)
+			{
+				Instantiate(goldCoin, transform.position, Quaternion.identity);
+			}
+		}
+		
 	}
 }
